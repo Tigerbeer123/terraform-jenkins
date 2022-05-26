80367b924d6ae9e304c3179ed2f4a2216fab8e4e
Tigerbeer123 <89633120+Tigerbeer123@users.noreply.github.com>
2022-05-26 10:04:27 +0800

    Update www-jenkins.tf

--- www-jenkins.tf
+++ www-jenkins.tf
@@ -104,6 +104,8 @@ resource "huaweicloud_compute_instance" "instance" {
     inline = [
       "chmod +x /tmp/install-jenkins.sh",
       "/tmp/install-jenkins.sh",
+          ]
+  }
   
 }
 
@@ -125,55 +127,3 @@ resource "huaweicloud_compute_eip_associate" "associated" {
   instance_id = huaweicloud_compute_instance.instance.id
 }
 
-
-
-resource "digitalocean_droplet" "www-jenkins" {
-  image              = "ubuntu-18-04-x64"
-  name               = "www-jenkins"
-  region             = "nyc3"
-  size               = "512mb"
-  private_networking = true
-  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
-
-  provisioner "file" {
-    source      = "install-jenkins.sh"
-    destination = "/tmp/install-jenkins.sh"
-  }
-
-  provisioner "file" {
-    source      = "install-plugins.sh"
-    destination = "/tmp/install-plugins.sh"
-  }
-
-#  provisioner "file" {
-#    source      = "plugins.txt"
-#    destination = "/tmp/plugins.txt"
-#  }
-
-  provisioner "file" {
-    source      = "jenkins.yaml"
-    destination = "/tmp/jenkins.yaml"
-  }
-
-  provisioner "file" {
-    content      = data.template_file.default_nginx_test.rendered
-    destination = "/tmp/jenkins-nginx-conf"
-  }  
-
-  connection {
-    user        = "root"
-    host        = digitalocean_droplet.www-jenkins.ipv4_address
-    type        = "ssh"
-    private_key = file(var.pvt_key)
-    timeout     = "2m"
-    agent       = false
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/install-jenkins.sh",
-      "/tmp/install-jenkins.sh",
-    ]
-  }
-
-}
