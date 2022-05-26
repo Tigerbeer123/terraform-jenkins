11e0565ee81abfe3182f0f0fb0ada941c940e3ad
Tigerbeer123 <89633120+Tigerbeer123@users.noreply.github.com>
2022-05-26 10:30:14 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,39 +2,23 @@ variable "template_name" {
   description = "template name"
   default = "jenkins"
   type        = string
-  validation {      
-      condition     = true
-      error_message = "param required"
-  }
 }
 
 variable "ecs_name" {
   description = "ecs name"
   default = "jenkins-ecs"
   type        = string
-  validation {      
-      condition     = true
-      error_message = "param required"
-  }
 }
 
 variable "ecs_flavor" {
   description = "flavor"
   default = "c6s.4xlarge.2"
   type        = string
-  validation {      
-      condition     = true
-      error_message = "param required"
-  }
 }
 
 variable "ecs_password" {
   description = "admin password"
   default  = "Abcd1234"
   type        = string
-  validation {      
-      condition     = true
-      error_message = "param required"
-  }
 }
 
